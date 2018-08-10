a9c0f4707c42037f5a27992ecf3915699b5e5dfe
micscoob <33203811+micscoob@users.noreply.github.com>
2018-08-10 10:58:33 -0500

    Update and rename flag.ps1 to America-Checker.ps1

--- America-Checker.ps1
+++ America-Checker.ps1
@@ -1,5 +1,6 @@
 #Flag
 #Merica. Need to figure out how to get the correct stars and stripes.
+#Checks to make sure the indivduals are american before running. 
 Function BlueRed 
 { Param($x,$y)
   $BlueReds = Write-Host "**********" -b blue -f white -nonewline; Write-Host "                 " -b red;
@@ -55,4 +56,30 @@ for($counter = 0; $counter -le 2; $counter++)
 
 
 echo "`n"
-Read-Host "Press ENTER"
+$Answer = Read-Host "ARE YOU AN AMERICAN?[Y][N]"
+
+$upperAnswer = $Answer.ToUpper()
+
+Switch ($upperAnswer)
+{
+  Y { Write-Host "WELLCOME FELLOW AMERICAN!!" }
+  YES { Write-Host "WELLCOME FELLOW AMERICAN!!" }
+  N { Write-Host "GET OUT OF HERE YA COMMIE!" ; Start-Sleep 3; exit }
+  NO { Write-Host "GET OUT OF HERE YA COMMIE!" ; Start-Sleep 3;  exit }
+  Default { Write-Host "YOU DIDN'T ANSWER YES OR NO. ONLY COMMIES CAN'T READ!"; Start-Sleep 3;  exit}
+ }
+
+While($true)
+{
+$Answer2 = Read-Host "PRESS ANY KEY TO RUN SCRIPT. [Q] TO QUIT "
+
+$upperAnswer2 = $Answer2.ToUpper()
+
+Switch ($upperAnswer2)
+{
+  Q { Write-Host "GOOD BYE!"; ]Start-Sleep 3; exit }
+  QUIT { Write-Host "GOOD BYE!"; ]Start-Sleep 3; exit }
+  Default { Write-Host "RUNNING SCIPT" }
+ }
+ 
+ }
